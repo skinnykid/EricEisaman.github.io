4239d792992630ab7ff2131198b18bf02a62f3f7
skinnykid <themartinfamily1.tm@gmail.com>
2017-04-28 11:39:11 -0400

    Update mod5.js

--- js/mod5.js
+++ js/mod5.js
@@ -5,12 +5,14 @@ window.onload = function(){
   canvas.height = 400;
   document.body.appendChild(canvas);
   var posX = canvas.width / 2,
-      posY = canvas.height / 2;
+      posY = canvas.height / 2,
+      velX = 3,
+      velY = 3;
   setInterval(function(){
     c.fillStyle = 'black';
     c.fillRect(0,0,canvas.width,canvas.height);
-    posX += 1;
-    posY += 1;
+    posX += velX;
+    posY += velY;
     c.fillStyle = 'white';
     c.fillRect(posX,posY,10,10);
   },30);
